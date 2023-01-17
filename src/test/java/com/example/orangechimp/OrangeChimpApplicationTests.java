251f5b40c3e601eb0c0b7672ad7b61f0e2f95cdd
Edward Moores <edwardmoores@me.com>
2023-01-17 10:37:14 +0000

    create project

---
+++ src/test/java/com/example/orangechimp/OrangeChimpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orangechimp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeChimpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
